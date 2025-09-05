9400ad60314d2964b23a781f0b3b9275887d0b07
RajSingh9876 <raj.singh_cs24@gla.ac.in>
2025-09-05 11:07:57 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Lecture {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int arr[]  = new int[len];
+
+        for (int i=0; i<len; i++){
+
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<len; i++){
+           if(arr[i]%2==0){
+               System.out.println("even");
+           }
+           else{
+               System.out.println("odd"+" ");
+           }
+
+            System.out.print(arr[i]+" ");
+
+
+        }
+
+    }
+}
